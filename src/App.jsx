18aee9221568f102a51071a1890e525ac1cfce4e
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-09 08:48:50 +0630

    setting up progress tracker page

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #b19cd9;
+`;
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`;
+
+const WeekdayTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`;
+
+function App() {
+  return (
+    <>
+      <Title>Progress Tracker</Title>
+      <div>
+        <Section>
+          <WeekdayTitle>M</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>W</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>F</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+      </div>
+    </>
+  );
+}
+
+export default App;
